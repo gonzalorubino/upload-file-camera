9990518024e5e63182022fc8fbee0e524b387067
Gonzalo <gonzalo.rubino@gmail.com>
2022-06-10 15:46:38 -0300

    Final Version

--- js/main.js
+++ js/main.js
@@ -35,48 +35,10 @@ document.forms.fileForm.addEventListener("submit", async (event) => {
         body: formData
     }).then(r => r.json())
     .then((json) => {
-        result.innerText = JSON.stringify(json);
-        result.classList.add('text-success');
+        console.log('json recibido: ', JSON.stringify(json));
         resultImg.src = json.secure_url;
-
-        return json;
-    })
-    .then(json => {
-        const newForm = new FormData();
-
-        newForm.set('name', 'upload-form')
-        newForm.set('data-netlify', 'true');
-        newForm.set('data', JSON.stringify(json));
-
-        fetch("/", {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                },
-                body: newForm,
-                method: "POST",
-            })
-            .then(() => {
-                console.log('success');
-            })
-            .catch((error) => {
-                console.log('error');
-            });
     })
     .catch((error) => {
         console.log('error!!', error);
     });
-
-    /** fetch("/", {
-            body: new FormData(event.target),
-            method: "POST",
-        })
-        .then(() => {
-            result.innerText = "Success";
-            result.classList.add('text-success');
-        })
-        .catch((error) => {
-            result.innerText = `Failed: ${error}`;
-            result.classList.add('text-danger');
-        });
-    */
 });
